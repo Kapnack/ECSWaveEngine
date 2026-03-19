b6ecded7968b3886616eb7bb42a4e33ce9818b45
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-19 07:15:43 -0300

    Update ECSTransform.cpp
    Fix dirty flag optimization

--- TSDV-WaveEngine/src/ECS/Transform/ECSTransform.cpp
+++ TSDV-WaveEngine/src/ECS/Transform/ECSTransform.cpp
@@ -209,6 +209,8 @@ namespace WaveEngine
 		model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0, 1, 0));
 		model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0, 0, 1));
 		model = glm::scale(model, glm::vec3(scale.x, scale.y, scale.z));
+
+		dirty = false;
 	}
 
 	const glm::mat4& ECSTransform::GetModel() const
