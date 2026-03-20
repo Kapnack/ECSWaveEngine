6ef48467139684c4d9305ed7e0a881cdb1dc51ed
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-20 03:23:01 -0300

    Update Renderer.cpp

--- TSDV-WaveEngine/src/Renderer/Renderer.cpp
+++ TSDV-WaveEngine/src/Renderer/Renderer.cpp
@@ -127,7 +127,7 @@ namespace WaveEngine
 		glEnableVertexAttribArray(3);
 
 		glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * MAX_INSTANCES, nullptr, GL_DYNAMIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(InstanceData) * MAX_INSTANCES, nullptr, GL_DYNAMIC_DRAW);
 
 		for (int i = 0; i < 4; i++)
 		{
@@ -137,12 +137,19 @@ namespace WaveEngine
 				4,
 				GL_FLOAT,
 				GL_FALSE,
-				sizeof(glm::mat4),
-				(void*)(i * sizeof(glm::vec4))
+				sizeof(InstanceData),
+				(void*)(offsetof(InstanceData, model) + i * sizeof(glm::vec4))
 			);
 
 			glVertexAttribDivisor(4 + i, 1);
 		}
+
+		glEnableVertexAttribArray(8);
+		glVertexAttribPointer(8, 4, GL_FLOAT, GL_FALSE,
+			sizeof(InstanceData),
+			(void*)offsetof(InstanceData, uvOffsetScale));
+
+		glVertexAttribDivisor(8, 1);
 
 		glBindVertexArray(0);
 	}
@@ -213,8 +220,8 @@ namespace WaveEngine
 			meshComp.meshID
 		};
 
-		batch.models.reserve(MAX_INSTANCES);
-		batch.models.push_back(transform.GetModel());
+		batch.instances.reserve(MAX_INSTANCES);
+		batch.instances.push_back({ transform.GetModel(),  glm::vec4(0.8f, 0.0f, 1.0f, 1.0f) });
 	}
 
 	void Renderer::Flush()
@@ -223,7 +230,7 @@ namespace WaveEngine
 
 		for (auto& [key, batch] : batching)
 		{
-			if (batch.models.empty())
+			if (batch.instances.empty())
 				continue;
 
 			Mesh& mesh = GetMeshManager()->Get(batch.batchData.meshID);
@@ -247,20 +254,20 @@ namespace WaveEngine
 			glBindVertexArray(batch.VAO);
 			glBindBuffer(GL_ARRAY_BUFFER, batch.instanceVBO);
 
-			if (batch.models.size() > batch.instanceCapacity)
+			if (batch.instances.size() > batch.instanceCapacity)
 			{
-				batch.instanceCapacity = batch.models.size();
+				batch.instanceCapacity = batch.instances.size();
 
 				glBufferData(GL_ARRAY_BUFFER,
-					batch.instanceCapacity * sizeof(glm::mat4),
+					batch.instanceCapacity * sizeof(InstanceData),
 					nullptr,
 					GL_DYNAMIC_DRAW);
 			}
 
 			glBufferSubData(GL_ARRAY_BUFFER,
 				0,
-				batch.models.size() * sizeof(glm::mat4),
-				batch.models.data());
+				batch.instances.size() * sizeof(InstanceData),
+				batch.instances.data());
 
 			Material* materialToUse =
 				GetMaterialManager()->GetMaterial(
@@ -287,14 +294,14 @@ namespace WaveEngine
 				mesh.GetIndexesSize(),
 				GL_UNSIGNED_INT,
 				0,
-				batch.models.size()
+				batch.instances.size()
 			);
 
 			materialToUse->UnBind();
 
 			++drawCalls;
 
-			batch.models.clear();
+			batch.instances.clear();
 		}
 	}
 
