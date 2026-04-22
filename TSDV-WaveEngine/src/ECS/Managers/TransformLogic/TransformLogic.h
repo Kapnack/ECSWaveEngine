312724384a1b0ae4c083495d95a0e6633b735cee
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-22 14:39:51 -0300

    Update TransformLogic.h

--- TSDV-WaveEngine/src/ECS/Managers/TransformLogic/TransformLogic.h
+++ TSDV-WaveEngine/src/ECS/Managers/TransformLogic/TransformLogic.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ECS/CompontRegistry/ComponentRegistry.h"
+#include <ECS/ComponentContainer/ComponentContainer.h>
+#include <glm/fwd.hpp>
+#include <ECS/Transform/ECSTransform.h>
 
 namespace WaveEngine
 {
@@ -16,5 +19,10 @@ namespace WaveEngine
 		void Update();
 
 		ComponentRegistry* GetComponentRegistry();
+
+		void UpdateHierarchy(
+			ComponentContainer<ECSTransform>& storage,
+			ECSTransform& transform,
+			const glm::mat4& parentMatrix);
 	};
 }
