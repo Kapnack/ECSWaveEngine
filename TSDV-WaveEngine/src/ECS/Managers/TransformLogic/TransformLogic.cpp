5008ed0aa4b97bbbe7068dd98387a543570ef7fd
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-22 14:39:54 -0300

    Update TransformLogic.cpp

--- TSDV-WaveEngine/src/ECS/Managers/TransformLogic/TransformLogic.cpp
+++ TSDV-WaveEngine/src/ECS/Managers/TransformLogic/TransformLogic.cpp
@@ -17,19 +17,48 @@ namespace WaveEngine
 	{
 	}
 
-	void TransformLogic::Update()
-	{
-		ComponentContainer<ECSTransform>& transforStorage = GetComponentRegistry()->CreateOrGetComponentStorage<ECSTransform>();
-		vector<ECSTransform>& transformComponents = transforStorage.GetComponents();
+    void TransformLogic::Update()
+    {
+        ComponentContainer<ECSTransform>& transforStorage =
+            GetComponentRegistry()->CreateOrGetComponentStorage<ECSTransform>();
 
-		for (ECSTransform& transform : transformComponents)
-		{
-			if (!transform.IsDirty())
-				continue;
+        vector<ECSTransform>& transformComponents =
+            transforStorage.GetComponents();
 
-			transform.CalculateTRS();
-		}
-	}
+        for (ECSTransform& transform : transformComponents)
+        {
+            if (!transform.IsDirty())
+                continue;
+
+            transform.CalculateTRS();
+        }
+
+        for (ECSTransform& transform : transformComponents)
+        {
+            if (transform.GetParent() == -1)
+            {
+                UpdateHierarchy(transforStorage, transform, glm::mat4(1.0f));
+            }
+        }
+    }
+
+    void TransformLogic::UpdateHierarchy(ComponentContainer<ECSTransform>& storage,
+        ECSTransform& transform,
+        const glm::mat4& parentMatrix)
+    {
+        glm::mat4 global = parentMatrix * transform.GetLocalModel();
+
+        transform.SetGlobalModel(global);
+
+        for (unsigned int childID : transform.GetChildren())
+        {
+            ECSTransform* child = storage.TryGet(childID);
+            if (!child)
+                continue;
+
+            UpdateHierarchy(storage, *child, global);
+        }
+    }
 
 	ComponentRegistry* TransformLogic::GetComponentRegistry()
 	{
