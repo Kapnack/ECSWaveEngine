9421958693b924e59f21c44ea9ec75d04b132761
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-05-08 19:56:07 -0300

    Update ModelImporter.cpp
    Improve ModelImporter InitFromScene

--- TSDV-WaveEngine/src/ModelImporter/ModelImporter.cpp
+++ TSDV-WaveEngine/src/ModelImporter/ModelImporter.cpp
@@ -26,105 +26,88 @@ namespace WaveEngine
 		return InitFromScene(assetName, pScene, filePath);
 	}
 
-    unsigned int ModelImporter::InitFromScene(
-        string_view assetName,
-        const aiScene* pScene,
-        const string_view filepath)
-    {
-        vector<VertexData> vertices;
-        vector<unsigned int> indices;
+	unsigned int ModelImporter::InitFromScene(
+		string_view assetName,
+		const aiScene* pScene,
+		const string_view filepath)
+	{
+		pair<int, int> count = GetVertexAndIndexSizes(*pScene);
 
-        unsigned int vertexOffset = 0;
+		VertexData* vertexToSubmit = new VertexData[count.first];
+		unsigned int* indicesToSubmit = new unsigned int[count.second];
 
-        for (unsigned int i = 0; i < pScene->mNumMeshes; ++i)
-        {
-            const aiMesh* mesh = pScene->mMeshes[i];
+		unsigned int vertexGlobalOffset = 0;
+		unsigned int indexGlobalOffset = 0;
 
-            for (unsigned int j = 0; j < mesh->mNumVertices; ++j)
-            {
-                VertexData vertex{};
+		for (unsigned int i = 0; i < pScene->mNumMeshes; ++i)
+		{
+			const aiMesh& mesh = *pScene->mMeshes[i];
 
-                vertex.position = Vector3(
-                    mesh->mVertices[j].x,
-                    mesh->mVertices[j].y,
-                    mesh->mVertices[j].z
-                );
+			for (unsigned int j = 0; j < mesh.mNumVertices; ++j)
+			{
+				VertexData& currentVertex = vertexToSubmit[vertexGlobalOffset + j];
 
-                if (mesh->HasNormals())
-                {
-                    vertex.normal = Vector3(
-                        mesh->mNormals[j].x,
-                        mesh->mNormals[j].y,
-                        mesh->mNormals[j].z
-                    );
-                }
-                else
-                {
-                    vertex.normal = Vector3(0, 1, 0);
-                }
+				currentVertex.position = Vector3(mesh.mVertices[j].x, mesh.mVertices[j].y, mesh.mVertices[j].z);
 
-                if (mesh->HasTextureCoords(0))
-                {
-                    vertex.textureCordinates = Vector2(
-                        mesh->mTextureCoords[0][j].x,
-                        mesh->mTextureCoords[0][j].y
-                    );
-                }
+				currentVertex.normal = mesh.HasNormals()
+					? Vector3(mesh.mNormals[j].x, mesh.mNormals[j].y, mesh.mNormals[j].z)
+					: Vector3(0, 1, 0);
 
-                if (mesh->HasVertexColors(0))
-                {
-                    aiColor4D color = mesh->mColors[0][j];
+				currentVertex.textureCordinates = mesh.HasTextureCoords(0)
+					? Vector2(mesh.mTextureCoords[0][j].x, mesh.mTextureCoords[0][j].y)
+					: Vector2(0, 0);
 
-                    vertex.color = Vector4(
-                        color.r,
-                        color.g,
-                        color.b,
-                        color.a
-                    );
-                }
-                else
-                {
-                    vertex.color = Vector4(1, 1, 1, 1);
-                }
+				if (mesh.HasVertexColors(0)) 
+				{
+					aiColor4D color = mesh.mColors[0][j];
+					currentVertex.color = Color(color.r, color.g, color.b, color.a);
+				}
+				else 
+				{
+					currentVertex.color = Color::White();
+				}
+			}
 
-                vertices.push_back(vertex);
-            }
+			for (unsigned int j = 0; j < mesh.mNumFaces; ++j)
+			{
+				const aiFace& face = mesh.mFaces[j];
 
-            for (unsigned int j = 0; j < mesh->mNumFaces; ++j)
-            {
-                const aiFace& face = mesh->mFaces[j];
+				for (unsigned int k = 0; k < face.mNumIndices; ++k)
+				{
+					indicesToSubmit[indexGlobalOffset++] = face.mIndices[k] + vertexGlobalOffset;
+				}
+			}
 
-                for (unsigned int k = 0; k < face.mNumIndices; ++k)
-                {
-                    indices.push_back(
-                        face.mIndices[k] + vertexOffset
-                    );
-                }
-            }
+			vertexGlobalOffset += mesh.mNumVertices;
+		}
 
-            vertexOffset += mesh->mNumVertices;
-        }
-
-        VertexData* vertexToSubmit = new VertexData[vertices.size()];
-        unsigned int* indicesToSubmit = new unsigned int[indices.size()];
-
-        for (int j = 0; j < vertices.size(); ++j)
-            vertexToSubmit[j] = vertices[j];
-        
-        for (int j = 0; j < indices.size(); ++j)
-            indicesToSubmit[j] = indices[j];
-
-        return GetMeshFactory()->CreateMesh(
-            assetName,
-            vertexToSubmit,
-            vertices.size(),
-            indicesToSubmit,
-            indices.size()
-        );
-    }
+		return GetMeshFactory()->CreateMesh(
+			assetName,
+			vertexToSubmit,
+			count.first,
+			indicesToSubmit,
+			count.second
+		);
+	}
 
 	MeshFactory* ModelImporter::GetMeshFactory()
 	{
 		return ServiceProvider::Instance().Get<MeshFactory>();
+	}
+
+	pair<unsigned int, unsigned int> ModelImporter::GetVertexAndIndexSizes(const aiScene& pScene)
+	{
+		unsigned int totalVertices = 0;
+		unsigned int totalIndices = 0;
+
+		for (unsigned int i = 0; i < pScene.mNumMeshes; ++i)
+		{
+			totalVertices += pScene.mMeshes[i]->mNumVertices;
+
+			for (unsigned int j = 0; j < pScene.mMeshes[i]->mNumFaces; ++j)
+				totalIndices += pScene.mMeshes[i]->mFaces[j].mNumIndices;
+		}
+
+		return pair<int, int>(totalVertices, totalIndices);
 	}
 }
