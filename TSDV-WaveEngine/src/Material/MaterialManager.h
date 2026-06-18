d7f1dc60c8cdc66c10e006e6668d96d43577336b
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-06-17 21:05:29 -0300

    Update MaterialManager.h

--- TSDV-WaveEngine/src/Material/MaterialManager.h
+++ TSDV-WaveEngine/src/Material/MaterialManager.h
@@ -16,8 +16,7 @@ namespace WaveEngine
 	class MaterialFactory;
 	class EntityManager;
 	class BaseGame;
-	class EntitiesImGui;
-	class MaterialsImGui;
+	class ImGuiClass;
 
 	WAVEEXPORT class MaterialManager final : public Service
 	{
@@ -29,8 +28,6 @@ namespace WaveEngine
 
 		void SaveMaterial(Material*& material);
 
-		unordered_map<unsigned int, Material*>& GetMaterials();
-
 		MaterialManager();
 		~MaterialManager();
 
@@ -39,8 +36,7 @@ namespace WaveEngine
 		friend class EntityManager;
 		friend class BaseGame;
 		friend class ServiceProvider;
-		friend class EntitiesImGui;
-		friend class MaterialsImGui;
+		friend class ImGuiClass;
 
 	public:
 
@@ -51,6 +47,8 @@ namespace WaveEngine
 		WAVEEXPORT void DeleteMaterial(const string_view name);
 
 		WAVEEXPORT void DeleteMaterial(const unsigned int& ID);
+
+		unordered_map<unsigned int, Material*>& GetMaterials();
 	};
 }
 
