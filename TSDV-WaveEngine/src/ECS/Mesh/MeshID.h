c17588425a2f7ce2b5d5e3237c380bac13c7eac4
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-01 13:16:10 -0300

    Update MeshID.h
    Add a Bounding Box to MeshID

--- TSDV-WaveEngine/src/ECS/Mesh/MeshID.h
+++ TSDV-WaveEngine/src/ECS/Mesh/MeshID.h
@@ -3,12 +3,15 @@
 #include "ECS/Component/Component.h"
 
 #include "Mesh/Mesh.h"
+#include "BoundingBox/BoundingBox.h"
 
 namespace WaveEngine
 {
 	struct MeshID : Component
 	{
 		unsigned int meshID = Mesh::NULL_MESH;
+
+		BoundingBox boundingBox;
 
 		MeshID(const unsigned int& ID) : Component(ID)
 		{ }
