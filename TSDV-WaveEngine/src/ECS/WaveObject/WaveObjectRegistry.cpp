f42b7ee6e3390459ae0e54f3154914d217ee4d7c
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-01 21:17:06 -0300

    Improve WaveObjectRegistry
    Make use of the EventSystem to keep a record of which WaveObjects are Parents, with the intent of optimizing the amount of iterations the DrawLogic and MeshLogic system have to do

--- TSDV-WaveEngine/src/ECS/WaveObject/WaveObjectRegistry.cpp
+++ TSDV-WaveEngine/src/ECS/WaveObject/WaveObjectRegistry.cpp
@@ -1,9 +1,40 @@
 #include "WaveObjectRegistry.h"
 
+#include <vector>
+#include <map>
+
+#include "EventSystem/EventSystem.h"
+#include "ServiceProvider/ServiceProvider.h"
+
 namespace WaveEngine
 {
 	WaveObjectRegistry::WaveObjectRegistry() : Service()
 	{
+	}
+
+	void WaveObjectRegistry::Init()
+	{
+		GetEventSystem()->Subscribe<ObjectBecameParentEvent>(this, &WaveObjectRegistry::OnObjectBecameParent);
+		GetEventSystem()->Subscribe<ObjectBecameChildEvent>(this, &WaveObjectRegistry::OnObjectBecameChild);
+	}
+
+	void WaveObjectRegistry::OnObjectBecameParent(const ObjectBecameParentEvent& objectBecameParentEvent)
+	{
+		for (unsigned int objectsID : parentsWaveObjects)
+			if (objectsID == objectBecameParentEvent.waveObjectID)
+				return;
+
+		parentsWaveObjects.push_back(objectBecameParentEvent.waveObjectID);
+	}
+
+	void WaveObjectRegistry::OnObjectBecameChild(const ObjectBecameChildEvent& objectBecameChildEvent)
+	{
+		erase(parentsWaveObjects, objectBecameChildEvent.waveObjectID);
+	}
+
+	EventSystem* WaveObjectRegistry::GetEventSystem()
+	{
+		return ServiceProvider::Instance().Get<EventSystem>();
 	}
 
 	void WaveObjectRegistry::AddObject(WaveObject*& newWaveObject)
@@ -13,6 +44,16 @@ namespace WaveEngine
 
 	map<unsigned int, WaveObject*>& WaveObjectRegistry::GetWaveObjects()
 	{
+		return waveObjects;
+	}
+
+	vector<WaveObject*> WaveObjectRegistry::GetParentWaveObjects()
+	{
+		vector<WaveObject*> waveObjects;
+
+		for (unsigned int waveObjectID : parentsWaveObjects)
+			waveObjects.push_back(&GetWaveObject(waveObjectID));
+
 		return waveObjects;
 	}
 
--- TSDV-WaveEngine/src/ECS/WaveObject/WaveObjectRegistry.h
+++ TSDV-WaveEngine/src/ECS/WaveObject/WaveObjectRegistry.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <vector>
 #include <map>
 
 #include "ServiceProvider/Service.h"
 #include "WaveObject.h"
+#include "EventSystem/Event.h"
+#include <EventSystem/EventSystem.h>
 
 using namespace std;
 
@@ -12,24 +15,73 @@ class ServiceProvider;
 
 namespace WaveEngine
 {
+	struct ObjectBecameParentEvent : Event
+	{
+		unsigned int waveObjectID = 0;
+
+		ObjectBecameParentEvent()
+		{ }
+
+		ObjectBecameParentEvent(unsigned int waveObjectID)
+		{
+			this->waveObjectID = waveObjectID;
+		}
+
+		void Reset() override
+		{
+			waveObjectID = 0;
+		}
+	};
+
+	struct ObjectBecameChildEvent : Event
+	{
+		unsigned int waveObjectID = 0;
+
+		ObjectBecameChildEvent()
+		{
+		}
+
+		ObjectBecameChildEvent(unsigned int waveObjectID)
+		{
+			this->waveObjectID = waveObjectID;
+		}
+
+		void Reset() override
+		{
+			waveObjectID = 0;
+		}
+	};
+
 	class WaveObjectRegistry : Service
 	{
 	private:
+
+		vector<unsigned int> parentsWaveObjects;
 
 		map<unsigned int, WaveObject*> waveObjects;
 
 		friend class BaseGame;
 		friend class ServiceProvider;
 
+		void OnObjectBecameParent(const ObjectBecameParentEvent& objectBecameParentEvent);
+		void OnObjectBecameChild(const ObjectBecameChildEvent& objectBecameChildEvent);
+
+		EventSystem* GetEventSystem();
+
 	public:
 
 		WaveObjectRegistry();
 		~WaveObjectRegistry();
 
+		void Init();
+
 		void AddObject(WaveObject*& newWaveObject);
 
 		map<unsigned int, WaveObject*>& GetWaveObjects();
 
+		vector<WaveObject*> GetParentWaveObjects();
+
 		WaveObject& GetWaveObject(const unsigned int& ID);
 	};
+
 }
