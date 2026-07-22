8a4563a8e7892ac2161cd6c539a277982ac9337d
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-22 04:10:02 -0300

    Update BoundingBox.cpp

--- TSDV-WaveEngine/src/BoundingBox/BoundingBox.cpp
+++ TSDV-WaveEngine/src/BoundingBox/BoundingBox.cpp
@@ -109,7 +109,7 @@ namespace WaveEngine
 	bool BoundingBox::Intersects(BoundingBox other) const
 	{
 		return std::abs(GetCenter().x - other.GetCenter().x) <= (GetSize().x + other.GetSize().x) * 0.5f &&
-			std::abs(GetCenter().y - other.GetSize().y) <= (GetSize().y + other.GetSize().y) * 0.5f &&
+			std::abs(GetCenter().y - other.GetCenter().y) <= (GetSize().y + other.GetSize().y) * 0.5f &&
 			std::abs(GetCenter().z - other.GetCenter().z) <= (GetSize().z + other.GetSize().z) * 0.5f;
 	}
 }
