2b29ba29c9ba6d5604ba5ffa75bae26a8e0cf386
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-23 04:26:58 -0300

    Update CameraManager.cpp

--- TSDV-WaveEngine/src/CameraManager/CameraManager.cpp
+++ TSDV-WaveEngine/src/CameraManager/CameraManager.cpp
@@ -5,10 +5,8 @@
 #include "ECS/ComponentContainer/ComponentContainer.h"
 #include "ECS/CompontRegistry/ComponentRegistry.h"
 #include "ServiceProvider/ServiceProvider.h"
-#include "ECS/Component/Component.h"
 #include "EventSystem/EventSystem.h"
 #include "ECS/Camera/Camera.h"
-#include "EventSystem/Event.h"
 
 namespace WaveEngine
 {
@@ -22,12 +20,56 @@ namespace WaveEngine
 		return ServiceProvider::Instance().Get<EventSystem>();
 	}
 
+	void CameraManager::Init()
+	{
+		GetEventSystem()->Subscribe<CameraChangeActiveStateEvent>(this, OnCameraActive);
+	}
+
+	void CameraManager::LateInit()
+	{
+	}
+
+	void CameraManager::OnCameraActive(const CameraChangeActiveStateEvent& event)
+	{
+		if (!event.isActive)
+		{
+			const vector<unsigned int>::iterator it = find(activeCamerasOrder.begin(), activeCamerasOrder.end(), event.cameraID);
+
+			if (it == activeCamerasOrder.end())
+				return;
+
+			const int index = static_cast<int>(distance(activeCamerasOrder.begin(), it));
+
+			activeCamerasOrder.erase(it);
+			RefreshOrderIndices(index);
+			return;
+		}
+
+		if (find(activeCamerasOrder.begin(), activeCamerasOrder.end(), event.cameraID) != activeCamerasOrder.end())
+			return;
+
+		const int index = min<int>(event.orderIndex, activeCamerasOrder.size());
+		activeCamerasOrder.insert(activeCamerasOrder.begin() + index, event.cameraID);
+		RefreshOrderIndices(index);
+	}
+
+	void CameraManager::RefreshOrderIndices(int from)
+	{
+		for (int i = from; i < activeCamerasOrder.size(); ++i)
+			GetCameraContainer().Get(activeCamerasOrder[i]).orderIndex = i;
+	}
+
 	vector<Camera*> CameraManager::GetActiveCameras()
 	{
 		vector<Camera*> cameras;
 
 		for (unsigned int cameraID : activeCamerasOrder)
+		{
+			if (cameraID == Component::NULL_COMPONENT)
+				continue;
+
 			cameras.push_back(&GetCameraContainer().Get(cameraID));
+		}
 
 		return cameras;
 	}
