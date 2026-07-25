cbf4d784e75b5254bd7d38592bffa857534d6d78
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-07-25 07:09:57 -0300

    Update Vector2.h

--- KapnackToolBox/src/WaveMath/Vector2/Vector2.h
+++ KapnackToolBox/src/WaveMath/Vector2/Vector2.h
@@ -22,6 +22,7 @@ WAVEEXPORT struct Vector2 final
 	WAVEEXPORT	void operator=(Vector3 vector3);
 
 	WAVEEXPORT	static Vector2 Zero();
+	WAVEEXPORT	static Vector2 One();
 	WAVEEXPORT	static Vector2 Up();
 	WAVEEXPORT	static Vector2 Down();
 	WAVEEXPORT	static Vector2 Right();
