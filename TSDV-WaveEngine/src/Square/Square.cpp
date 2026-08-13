2f771613274f8173849627d248c069a102426e5a
Kapnack <unknowrealperson@gmail.com>
2026-08-13 20:32:19 -0300

    Improve Square

---
+++ TSDV-WaveEngine/src/Square/Square.cpp
@@ -0,0 +1,49 @@
+#include "Square.h"
+
+namespace WaveEngine
+{
+	Square::Square()
+	{
+	}
+
+	Square::Square(Vector2 center, Vector2 size)
+	{
+		SetMinMax(center - size * 0.5f, center + size * 0.5f);
+	}
+
+	void Square::Encapsulate(Square square)
+	{
+		SetMinMax(Vector2::Min(min, square.min), Vector2::Max(max, square.max));
+	}
+
+	void Square::SetMinMax(Vector2 vector2A, Vector2 vector2B)
+	{
+		min = Vector2::Min(vector2A, vector2B);
+		max = Vector2::Max(vector2A, vector2B);
+	}
+
+	void Square::SetMin(Vector2 min)
+	{
+		this->min = min;
+	}
+
+	void Square::SetMax(Vector2 max)
+	{
+		this->max = max;
+	}
+
+	Vector2 Square::GetSize() const
+	{
+		return max - min;
+	}
+
+	Vector2 Square::GetCenter() const
+	{
+		return (max + min) * 0.5f;
+	}
+
+	Vector2 Square::GetExtends() const
+	{
+		return (max - min) * 0.5f;
+	}
+}
--- TSDV-WaveEngine/src/Square/Square.h
+++ TSDV-WaveEngine/src/Square/Square.h
@@ -6,10 +6,28 @@ namespace WaveEngine
 {
 	struct Square
 	{
-		Vector2 position = Vector2::Zero();
-		Vector2 size = Vector2::One();
+	private:
 
 		Vector2 min = -Vector2::One() * 0.5f;
 		Vector2 max = Vector2::One() * 0.5f;
+
+	public:
+
+		Square();
+		Square(Vector2 center, Vector2 size);
+
+		void Encapsulate(Square square);
+
+		void SetMinMax(Vector2 vector2A, Vector2 vector2B);
+
+		void SetMin(Vector2 min);
+		void SetMax(Vector2 max);
+
+		Vector2 GetSize() const;
+		Vector2 GetExtends() const;
+		Vector2 GetCenter() const;
+
+		Vector2 GetMin() const;
+		Vector2 GetMax() const;
 	};
 }
