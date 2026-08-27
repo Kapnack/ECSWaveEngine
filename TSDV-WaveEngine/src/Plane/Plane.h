d006b8052c78cebeecc304dc6f4a21bbf306b38f
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-08-26 21:49:07 -0300

    Create Plane.h

---
+++ TSDV-WaveEngine/src/Plane/Plane.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "WaveMath/Vector3/Vector3.h"
+
+namespace WaveEngine
+{
+	struct Plane
+	{
+	private:
+
+		Vector3 normal;
+		float originDistance;
+
+	public:
+
+		Plane();
+		Plane(Vector3 normal);
+		Plane(Vector3 normal, Vector3 center);
+		static Plane BisectorPlane(Vector3 positionA, Vector3 positionB);
+
+		float SignedDistance(Vector3 position) const;
+
+		void SetNormal(Vector3 normal);
+		void SetPosition(Vector3 position);
+
+		bool operator==(Plane other) const;
+	};
+}
