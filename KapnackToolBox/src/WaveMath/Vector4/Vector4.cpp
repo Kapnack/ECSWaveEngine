d32d434c1de78373dc7393689227e4175e4957b9
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-06 19:29:21 -0300

    Improve Vector4
    Add new static methods to return basic and useful Vector4, also replace the const float reference in the constructors to improve Vector4 instantiation

--- KapnackToolBox/src/WaveMath/Vector4/Vector4.cpp
+++ KapnackToolBox/src/WaveMath/Vector4/Vector4.cpp
@@ -1,13 +1,51 @@
 #include "Vector4.h"
+#include "Vector4.h"
+
+#include <cfloat>
 
 Vector4::Vector4()
 {
 }
 
-Vector4::Vector4(const float& x, const float& y, const float& z, const float& w)
+Vector4::Vector4(float x, float y, float z, float w)
 {
 	this->x = x;
 	this->y = y;
 	this->z = z;
 	this->w = w;
+}
+
+Vector4 Vector4::Zero()
+{
+	return Vector4(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+Vector4 Vector4::One()
+{
+	return Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+Vector4 Vector4::Min()
+{
+	return Vector4(FLT_MIN, FLT_MIN, FLT_MIN, FLT_MIN);
+}
+
+Vector4 Vector4::NMin()
+{
+	return -Min();
+}
+
+Vector4 Vector4::Max()
+{
+	return Vector4(FLT_MAX, FLT_MAX, FLT_MAX, FLT_MAX);
+}
+
+Vector4 Vector4::NMax()
+{
+	return -Max();
+}
+
+Vector4 Vector4::operator-() const
+{
+	return Vector4(-x, -y, -z, -w);
 }
--- KapnackToolBox/src/WaveMath/Vector4/Vector4.h
+++ KapnackToolBox/src/WaveMath/Vector4/Vector4.h
@@ -2,13 +2,22 @@
 
 #include "Export.h"
 
-WAVEEXPORT struct Vector4
+struct WAVEEXPORT Vector4
 {
 	float x = 0.0f;
 	float y = 0.0f;
 	float z = 0.0f;
 	float w = 0.0f;
 
-	WAVEEXPORT Vector4();
-	WAVEEXPORT Vector4(const float& r, const float& g, const float& b, const float& a);
+	Vector4();
+	Vector4(float x, float y, float z, float w);
+
+	static Vector4 Zero();
+	static Vector4 One();
+	static Vector4 Min();
+	static Vector4 NMin();
+	static Vector4 Max();
+	static Vector4 NMax();
+
+	Vector4 operator-() const;
 };
