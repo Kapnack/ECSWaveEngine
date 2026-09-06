9bff0a7f0299efc423d99fa8c74037d42bbb3af5
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-06 20:57:41 -0300

    Create WaveBehaviour.h
    Starting WaveBehaviour

---
+++ TSDV-WaveEngine/src/ECS/WaveBehaviour/WaveBehaviour.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ECS/Component/Component.h"
+
+namespace WaveEngine
+{
+	class WaveBehaviour : public Component
+	{
+	public:
+
+		virtual void Init()
+		{
+		}
+
+		virtual void LateInit()
+		{
+		}
+
+		virtual void Update()
+		{
+		}
+
+		virtual void LateUpdate()
+		{
+		}
+
+		virtual void Destroy()
+		{
+		}
+	};
+}
