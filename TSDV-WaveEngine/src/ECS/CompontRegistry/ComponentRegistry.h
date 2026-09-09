c61903bc9f42b2cd2327d21a9f9d3a45d6db524d
Kapnack <unknowrealperson@gmail.com>
2026-09-08 22:09:11 -0300

    You can do it Nakamura

--- TSDV-WaveEngine/src/ECS/CompontRegistry/ComponentRegistry.h
+++ TSDV-WaveEngine/src/ECS/CompontRegistry/ComponentRegistry.h
@@ -25,127 +25,35 @@ namespace WaveEngine
 
 	public:
 
-		ComponentRegistry() : Service()
-		{
-		}
-
-		~ComponentRegistry()
-		{
-			for (unordered_map<type_index, IStorage*>::iterator storage = storages.begin(); storage != storages.end(); ++storage)
-				delete storage->second;
-		}
+		ComponentRegistry();
+		~ComponentRegistry();
 
 		template<TypeComponent T>
-		T& AddComponent(unsigned int entity)
-		{
-			type_index typeIndex = typeid(T);
-
-			if constexpr (std::is_base_of_v<WaveBehaviour, T>)
-				waveBehavioursByEntityID[entity].push_back(typeIndex);
-
-			if (!storages.contains(typeIndex))
-				storages[typeIndex] = new ComponentContainer<T>();
-
-			static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Add(entity);
-
-			return static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Get(entity);
-		}
+		T& AddComponent(unsigned int entity);
 
 		template<TypeComponent T>
-		bool HasStorage()
-		{
-			return storages.find(typeid(T)) != storages.end();
-		}
+		bool HasStorage();
+		template<TypeComponent T>
+		bool Has(unsigned int entity);
+		template<TypeComponent T>
+		T& Get(unsigned int entity);
+
+		vector<WaveBehaviour*> GetBehaviours();
 
 		template<TypeComponent T>
-		bool Has(unsigned int entity)
-		{
-			return GetComponentStorage<T>().Has(entity);
-		}
+		T* TryGet(unsigned int entity);
+		template<TypeComponent T>
+		void RemoveComponent(unsigned int entity);
 
 		template<TypeComponent T>
-		T& Get(unsigned int entity)
-		{
-			return GetComponentStorage<T>().Get(entity);
-		}
-
-		vector<WaveBehaviour*> GetBehaviours()
-		{
-			vector<WaveBehaviour*> waveBehaviours;
-
-			for (const pair<const unsigned int, vector<type_index>>& entry : waveBehavioursByEntityID)
-			{
-				unsigned int entity = entry.first;
-				const vector<type_index>& typeIndices = entry.second;
-
-				for (const type_index& typeIndex : typeIndices)
-				{
-					auto storageIt = storages.find(typeIndex);
-					if (storageIt == storages.end())
-						continue;
-
-					if (WaveBehaviour* behaviour = storageIt->second->GetAsWaveBehaviour(entity))
-						waveBehaviours.push_back(behaviour);
-				}
-			}
-
-			return waveBehaviours;
-		}
+		ComponentContainer<T>& GetComponentStorage();
+		template<TypeComponent T>
+		ComponentContainer<T>& CreateOrGetComponentStorage();
 
 		template<TypeComponent T>
-		T* TryGet(unsigned int entity)
-		{
-			return GetComponentStorage<T>().TryGet(entity);
-		}
+		ComponentContainer<T>* TryGetComponentStorage();
 
 		template<TypeComponent T>
-		void RemoveComponent(unsigned int entity)
-		{
-			type_index typeIndex = typeid(T);
-
-			if (!storages.contains(typeIndex))
-				return;
-
-			static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Remove(entity);
-		}
-
-		template<TypeComponent T>
-		ComponentContainer<T>& GetComponentStorage()
-		{
-			return *static_cast<ComponentContainer<T>*>(storages.at(typeid(T)));
-		}
-
-		template<TypeComponent T>
-		ComponentContainer<T>& CreateOrGetComponentStorage()
-		{
-			type_index typeIndex = typeid(T);
-
-			if (!storages.contains(typeIndex))
-			{
-				ComponentContainer<T>* newStorage = new ComponentContainer<T>();
-				storages[typeIndex] = newStorage;
-
-				return *newStorage;
-			}
-
-			return *static_cast<ComponentContainer<T>*>(storages.at(typeid(T)));
-		}
-
-		template<TypeComponent T>
-		ComponentContainer<T>* TryGetComponentStorage()
-		{
-			type_index typeIndex = typeid(T);
-
-			if (!storages.contains(typeIndex))
-				return nullptr;
-
-			return static_cast<ComponentContainer<T>*>(storages.at(typeIndex));
-		}
-
-		template<TypeComponent T>
-		T& GetComponent(unsigned int entity)
-		{
-			return GetComponentStorage<T>().Get(entity);
-		}
+		T& GetComponent(unsigned int entity);
 	};
 }
---
+++ TSDV-WaveEngine/src/ECS/CompontRegistry/ComponentRegistry.tpp
@@ -0,0 +1,133 @@
+#ifndef COMPONENT_REGISTRY
+#define COMPONENT_REGISTRY
+
+#include "ComponentRegistry.h"
+#include "ECS/WaveBehaviour/WaveBehaviour.h"
+
+namespace WaveEngine
+{
+	ComponentRegistry::ComponentRegistry() : Service()
+	{
+	}
+
+	ComponentRegistry::~ComponentRegistry()
+	{
+		for (unordered_map<type_index, IStorage*>::iterator storage = storages.begin(); storage != storages.end(); ++storage)
+			delete storage->second;
+	}
+
+	template<TypeComponent T>
+	T& ComponentRegistry::AddComponent(unsigned int entity)
+	{
+		type_index typeIndex = typeid(T);
+
+		if constexpr (std::is_base_of_v<WaveBehaviour, T>)
+			waveBehavioursByEntityID[entity].push_back(typeIndex);
+
+		if (!storages.contains(typeIndex))
+			storages[typeIndex] = new ComponentContainer<T>();
+
+		static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Add(entity);
+
+		return static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Get(entity);
+	}
+
+	template<TypeComponent T>
+	bool ComponentRegistry::HasStorage()
+	{
+		return storages.find(typeid(T)) != storages.end();
+	}
+
+	template<TypeComponent T>
+	bool ComponentRegistry::Has(unsigned int entity)
+	{
+		return GetComponentStorage<T>().Has(entity);
+	}
+
+	template<TypeComponent T>
+	T& ComponentRegistry::Get(unsigned int entity)
+	{
+		return GetComponentStorage<T>().Get(entity);
+	}
+
+	vector<WaveBehaviour*> ComponentRegistry::GetBehaviours()
+	{
+		vector<WaveBehaviour*> waveBehaviours;
+
+		for (const pair<const unsigned int, vector<type_index>>& entry : waveBehavioursByEntityID)
+		{
+			unsigned int entity = entry.first;
+			const vector<type_index>& typeIndices = entry.second;
+
+			for (const type_index& typeIndex : typeIndices)
+			{
+				auto storageIt = storages.find(typeIndex);
+				if (storageIt == storages.end())
+					continue;
+
+				if (WaveBehaviour* behaviour = storageIt->second->GetAsWaveBehaviour(entity))
+					waveBehaviours.push_back(behaviour);
+			}
+		}
+
+		return waveBehaviours;
+	}
+
+	template<TypeComponent T>
+	T* ComponentRegistry::TryGet(unsigned int entity)
+	{
+		return GetComponentStorage<T>().TryGet(entity);
+	}
+
+	template<TypeComponent T>
+	void ComponentRegistry::RemoveComponent(unsigned int entity)
+	{
+		type_index typeIndex = typeid(T);
+
+		if (!storages.contains(typeIndex))
+			return;
+
+		static_cast<ComponentContainer<T>*>(storages.at(typeIndex))->Remove(entity);
+	}
+
+	template<TypeComponent T>
+	ComponentContainer<T>& ComponentRegistry::GetComponentStorage()
+	{
+		return *static_cast<ComponentContainer<T>*>(storages.at(typeid(T)));
+	}
+
+	template<TypeComponent T>
+	ComponentContainer<T>& ComponentRegistry::CreateOrGetComponentStorage()
+	{
+		type_index typeIndex = typeid(T);
+
+		if (!storages.contains(typeIndex))
+		{
+			ComponentContainer<T>* newStorage = new ComponentContainer<T>();
+			storages[typeIndex] = newStorage;
+
+			return *newStorage;
+		}
+
+		return *static_cast<ComponentContainer<T>*>(storages.at(typeid(T)));
+	}
+
+	template<TypeComponent T>
+	ComponentContainer<T>* ComponentRegistry::TryGetComponentStorage()
+	{
+		type_index typeIndex = typeid(T);
+
+		if (!storages.contains(typeIndex))
+			return nullptr;
+
+		return static_cast<ComponentContainer<T>*>(storages.at(typeIndex));
+	}
+
+	template<TypeComponent T>
+	T& GetComponent(unsigned int entity)
+	{
+		return GetComponentStorage<T>().Get(entity);
+	}
+}
+
+#endif
