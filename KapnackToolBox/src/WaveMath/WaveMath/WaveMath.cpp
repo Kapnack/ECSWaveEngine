6d68a6e445a469242a738085cf3221bfd5c1a617
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-24 01:19:29 -0300

    Improve WaveMath
    Improve Clamp by replacing the lambdas conditions for bit operations, now it avoids the Arithmetic Logic Unit in the CPU.

--- KapnackToolBox/src/WaveMath/WaveMath/WaveMath.cpp
+++ KapnackToolBox/src/WaveMath/WaveMath/WaveMath.cpp
@@ -1,5 +1,7 @@
 #include "WaveMath.h"
+
 #include <bit>
+#include <cstdint>
 
 float WaveMath::Epsilon()
 {
@@ -16,17 +18,11 @@ bool WaveMath::Approximately(float a, float b)
 	return Abs(b - a) < Epsilon();
 }
 
-float WaveMath::Abs(float number)
+short WaveMath::Abs(short number)
 {
-#ifdef _WIN32
-	uint32_t bits = std::bit_cast<uint32_t>(number);
-#elif _WIN64
-	uint64_t bits = std::bit_cast<uint64_t>(number);
-#endif
+	short mask = number >> 15;
 
-	bits &= 0x7FFFFFFF;
-
-	return std::bit_cast<float>(bits);
+	return (number + mask) ^ mask;
 }
 
 int WaveMath::Abs(int number)
@@ -36,29 +32,79 @@ int WaveMath::Abs(int number)
 	return (number + mask) ^ mask;
 }
 
-float WaveMath::Opposite(float number)
+float WaveMath::Abs(float number)
 {
-#ifdef _WIN32
 	uint32_t bits = std::bit_cast<uint32_t>(number);
-#elif _WIN64
-	uint64_t bits = std::bit_cast<uint64_t>(number);
-#endif
 
-	bits ^= 0x80000000;
+	bits &= 0x7FFFFFFF;
 
 	return std::bit_cast<float>(bits);
 }
 
+double WaveMath::Abs(double number)
+{
+	uint64_t bits = std::bit_cast<uint64_t>(number);
+
+	bits &= 0x7FFFFFFFFFFFFFFFULL;
+
+	return std::bit_cast<double>(bits);
+}
+
+short WaveMath::Clamp(short value, short min, short max)
+{
+	return Min(Max(value, min), max);
+}
+
+int WaveMath::Clamp(int value, int min, int max)
+{
+	return Min(Max(value, min), max);
+}
+
+float WaveMath::Clamp01(float value)
+{
+	return Clamp(value, 0.0f, 1.0f);
+}
+
+float WaveMath::Clamp(float value, float min, float max)
+{
+	return Min(Max(value, min), max);
+}
+
+double WaveMath::Clamp01(double value)
+{
+	return Clamp(value, 0.0, 1.0);
+}
+
+double WaveMath::Clamp(double value, double min, double max)
+{
+	return Min(Max(value, min), max);
+}
+
 float WaveMath::Sqr(float number)
 {
-	if (number < 0)
-		return 0;
-
-	if (number == 0)
+	if (number < 0 || number == 0)
 		return 0;
 
 	float guess = number * 0.5f;
 	float betterGuess = 0;
+
+	while (WaveMath::Abs(guess - betterGuess) > WaveMath::Epsilon())
+	{
+		betterGuess = guess;
+
+		guess = WaveMath::Abs(guess + number / guess) * 0.5f;
+	}
+
+	return betterGuess;
+}
+
+double WaveMath::Sqr(double number)
+{
+	if (number < 0 || number == 0)
+		return 0;
+
+	double guess = number * 0.5f;
+	double betterGuess = 0;
 
 	while (WaveMath::Abs(guess - betterGuess) > WaveMath::Epsilon())
 	{
@@ -80,13 +126,88 @@ float WaveMath::Rad2Deg(float number)
 	return number * (180.0f / PI());
 }
 
+short WaveMath::Max(short a, short b)
+{
+	return a - ((a - b) & ((a - b) >> 15));
+}
+
+short WaveMath::Min(short a, short b)
+{
+	return b + ((a - b) & ((a - b) >> 15));
+}
+
+int WaveMath::Max(int a, int b)
+{
+	return a - ((a - b) & ((a - b) >> 31));
+}
+
 int WaveMath::Min(int a, int b)
 {
 	return b + ((a - b) & ((a - b) >> 31));
+}
+
+float WaveMath::Max(float a, float b)
+{
+	uint32_t bits = std::bit_cast<uint32_t>(a - b);
+
+	uint32_t sign = bits & 0x7FFFFFFF;
+
+	return a - (bits & (sign));
+}
+
+float WaveMath::Min(float a, float b)
+{
+	uint32_t bits = std::bit_cast<uint32_t>(a - b);
+
+	uint32_t sign = bits & 0x7FFFFFFF;
+
+	return b + (bits & (sign));
+}
+
+double WaveMath::Max(double a, double b)
+{
+	uint64_t bits = std::bit_cast<uint64_t>(a - b);
+
+	uint64_t sign = bits & 0x7FFFFFFFFFFFFFFFULL;
+
+	return a - (bits & (sign));
+}
+
+double WaveMath::Min(double a, double b)
+{
+	uint64_t bits = std::bit_cast<uint64_t>(a - b);
+
+	uint64_t sign = bits & 0x7FFFFFFFFFFFFFFFULL;
+
+	return b + (bits & (sign));
+}
+
+short WaveMath::Opposite(short number)
+{
+	const short mask = -1;
+	return (number + mask) ^ mask;
 }
 
 int WaveMath::Opposite(int number)
 {
 	const int mask = -1;
 	return (number + mask) ^ mask;
+}
+
+float WaveMath::Opposite(float number)
+{
+	uint32_t bits = std::bit_cast<uint32_t>(number);
+
+	bits ^= 0x80000000;
+
+	return std::bit_cast<float>(bits);
+}
+
+double WaveMath::Opposite(double number)
+{
+	uint64_t bits = std::bit_cast<uint64_t>(number);
+
+	bits ^= 0x8000000000000000ULL;
+
+	return std::bit_cast<double>(bits);
 }
--- KapnackToolBox/src/WaveMath/WaveMath/WaveMath.h
+++ KapnackToolBox/src/WaveMath/WaveMath/WaveMath.h
@@ -16,20 +16,17 @@ public:
 
 	static bool Approximately(float a, float b);
 
-	static float Abs(float number);
+	static short Abs(short number);
 	static int Abs(int number);
+	static float Abs(float number);
+	static double Abs(double number);
 
-	template<SignableObject T>
-	static T Clamp(T value, T min, T max)
-	{
-		return value < min ? min : value > max ? max : value;
-	}
-
-	template<SignableObject T>
-	static T Clamp01(T value)
-	{
-		return Clamp(value, 0.0f, 1.0f);
-	}
+	static short Clamp(short value, short min, short max);
+	static int Clamp(int value, int min, int max);
+	static float Clamp01(float value);
+	static float Clamp(float value, float min, float max);
+	static double Clamp01(double value);
+	static double Clamp(double value, double min, double max);
 
 	template<SignableObject T>
 	static int Sign(T number)
@@ -37,12 +34,22 @@ public:
 		return static_cast<int>((number > 0) - (number < 0));
 	}
 
+	static short Max(short a, short b);
+	static short Min(short a, short b);
+	static int Max(int a, int b);
 	static int Min(int a, int b);
+	static float Max(float a, float b);
+	static float Min(float a, float b);
+	static double Max(double a, double b);
+	static double Min(double a, double b);
 
+	static short Opposite(short number);
 	static int Opposite(int number);
 	static float Opposite(float number);
+	static double Opposite(double number);
 
 	static float Sqr(float number);
+	static double Sqr(double number);
 
 	static float Deg2Rad(float number);
 	static float Rad2Deg(float number);
