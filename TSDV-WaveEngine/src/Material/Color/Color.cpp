e5d20cadc685034843d29c3bf8d00bb2c9722289
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-09-24 04:19:50 -0300

    Improve Color
    Add operators to Color and new Constructors

--- TSDV-WaveEngine/src/Material/Color/Color.cpp
+++ TSDV-WaveEngine/src/Material/Color/Color.cpp
@@ -1,5 +1,8 @@
 #include "Color.h"
 
+#include "WaveMath/Vector4/Vector4.h"
+#include "WaveMath/WaveMath/WaveMath.h"
+
 namespace WaveEngine
 {
 	float Color::GetColor32MaxValue()
@@ -23,6 +26,22 @@ namespace WaveEngine
 		this->a = color.a;
 	}
 
+	Color::Color(Vector3 vector)
+	{
+		this->r = vector.x;
+		this->g = vector.y;
+		this->b = vector.z;
+		this->a = 1.0f;
+	}
+
+	Color::Color(Vector4 vector)
+	{
+		this->r = vector.x;
+		this->g = vector.y;
+		this->b = vector.z;
+		this->a = vector.w;
+	}
+
 	Color::Color(float r, float g, float b)
 	{
 		this->r = r;
@@ -39,9 +58,39 @@ namespace WaveEngine
 		this->a = a;
 	}
 
+	void Color::Normalize()
+	{
+		*this = Normalized();
+	}
+
+	Color Color::Normalized() const
+	{
+		return Normalized(*this);
+	}
+
+	float Color::Magnitude() const
+	{
+		return Magnitude(*this);
+	}
+
+	float Color::SqrMagnitude() const
+	{
+		return SqrMagnitude(*this);
+	}
+
+	bool Color::Approximately(Color other) const
+	{
+		return Approximately(*this, other);
+	}
+
+	Color Color::ColorFromColor255(Color color)
+	{
+		return ColorFromColor255(color.r, color.g, color.b, color.a);
+	}
+
 	Color Color::ColorFromColor255(float r, float g, float b)
 	{
-		return Color(r / GetColor32MaxValue(), g / GetColor32MaxValue(), b / GetColor32MaxValue(), GetColor32MaxValue());
+		return Color(r / GetColor32MaxValue(), g / GetColor32MaxValue(), b / GetColor32MaxValue());
 	}
 
 	Color Color::ColorFromColor255(float r, float g, float b, float a)
@@ -51,12 +100,22 @@ namespace WaveEngine
 
 	Color Color::GetAsColor255()
 	{
-		return GetAsColor255(*this);
+		return Color255FromColor(*this);
 	}
 
-	Color Color::GetAsColor255(Color color)
+	Color Color::Color255FromColor(Color color)
 	{
-		return Color(color.r * GetColor32MaxValue(), color.g * GetColor32MaxValue(), color.b * GetColor32MaxValue(), color.a * GetColor32MaxValue());
+		return Color255FromColor(color.r, color.g, color.b, color.a);
+	}
+
+	Color Color::Color255FromColor(float r, float g, float b)
+	{
+		return Color(r * GetColor32MaxValue(), g * GetColor32MaxValue(), b * GetColor32MaxValue(), GetColor32MaxValue());
+	}
+
+	Color Color::Color255FromColor(float r, float g, float b, float a)
+	{
+		return Color(r * GetColor32MaxValue(), g * GetColor32MaxValue(), b * GetColor32MaxValue(), a * GetColor32MaxValue());
 	}
 
 	Color Color::White()
@@ -92,5 +151,133 @@ namespace WaveEngine
 	Color Color::Clear()
 	{
 		return Color(0.0f, 0.0f, 0.0f, 0.0f);
+	}
+
+	Color Color::operator+(Color other) const
+	{
+		return Color(r + other.r, g + other.g, b + other.b, a + other.a);
+	}
+
+	void Color::operator+=(Color other)
+	{
+		*this = *this + other;
+	}
+
+	Color Color::operator-(Color other) const
+	{
+		return Color(r - other.r, g - other.g, b - other.b, a - other.a);
+	}
+
+	void Color::operator-=(Color other)
+	{
+		*this = *this - other;
+	}
+
+	Color Color::operator*(Color other) const
+	{
+		return Color(r * other.r, g * other.g, b * other.b, a * other.a);
+	}
+
+	Color Color::operator*(float scalar) const
+	{
+		return Color(r * scalar, g * scalar, b * scalar, a * scalar);
+	}
+
+	void Color::operator*=(Color other)
+	{
+		*this = *this * other;
+	}
+
+	void Color::operator*=(float scalar)
+	{
+		*this = *this * scalar;
+	}
+
+	Color Color::operator/(Color other) const
+	{
+		return Color(r / other.r, g / other.g, b / other.b, a / other.a);
+	}
+
+	Color Color::operator/(float scalar) const
+	{
+		return Color(r / scalar, g / scalar, b / scalar, a / scalar);
+	}
+
+	void Color::operator/=(Color other)
+	{
+		*this = *this / other;
+	}
+
+	void Color::operator/=(float scalar)
+	{
+		*this = *this / scalar;
+	}
+
+	bool Color::operator>(Color other) const
+	{
+		return r > other.r && g > other.g && b > other.b && a > other.a;
+	}
+
+	bool Color::operator<(Color other) const
+	{
+		return other > *this;
+	}
+
+	bool Color::operator==(Color other) const
+	{
+		return r == other.r && g == other.g && b == other.b && a == other.a;
+	}
+
+	bool Color::operator!=(Color other) const
+	{
+		return !(*this == other);
+	}
+
+	Color::operator Vector4() const
+	{
+		return Vector4(r, g, b, a);
+	}
+
+	Color Color::Normalized(Color color)
+	{
+		float mag = color.Magnitude();
+
+		if (mag < WaveMath::Epsilon())
+			return Color::Clear();
+
+		if (WaveMath::Abs(mag - 1.0f) < WaveMath::Epsilon())
+			return color;
+
+		return color / mag;
+	}
+
+	float Color::Magnitude(Color color)
+	{
+		return WaveMath::SqrF(SqrMagnitude(color));
+	}
+
+	float Color::SqrMagnitude(Color color)
+	{
+		return Dot(color, color);
+	}
+
+	float Color::Dot(Color a, Color b)
+	{
+		return a.r * b.r + a.g * b.g + a.b * b.b + a.a * b.a;
+	}
+
+	bool Color::Approximately(Color a, Color b)
+	{
+		return WaveMath::Approximately(a.r, b.r) && WaveMath::Approximately(a.g, b.g) && WaveMath::Approximately(a.b, b.b) && WaveMath::Approximately(a.a, b.a);
+	}
+
+	Color operator*(float scalar, Color color)
+	{
+		return color * scalar;
+	}
+
+	Color operator/(float scalar, Color color)
+	{
+		return color / scalar;
 	}
 }
--- TSDV-WaveEngine/src/Material/Color/Color.h
+++ TSDV-WaveEngine/src/Material/Color/Color.h
@@ -2,6 +2,9 @@
 
 #include "Export.h"
 
+#include "WaveMath/Vector3/Vector3.h"
+#include "WaveMath/Vector4/Vector4.h"
+
 namespace WaveEngine
 {
 	struct WAVEEXPORT Color
@@ -18,33 +21,60 @@ namespace WaveEngine
 		float a;
 
 		Color();
-
 		Color(const Color& color);
-
+		Color(Vector3 vector);
+		Color(Vector4 vector);
 		Color(float r, float g, float b);
-
 		Color(float r, float g, float b, float a);
 
-		static Color ColorFromColor255(float r, float g, float b);
+		void Normalize();
+		Color Normalized() const;
+		float Magnitude() const;
+		float SqrMagnitude() const;
+		bool Approximately(Color other) const;
 
+		static Color ColorFromColor255(Color color);
+		static Color ColorFromColor255(float r, float g, float b);
 		static Color ColorFromColor255(float r, float g, float b, float a);
 
 		Color GetAsColor255();
-
-		static Color GetAsColor255(Color color);
+		static Color Color255FromColor(Color color);
+		static Color Color255FromColor(float r, float g, float b);
+		static Color Color255FromColor(float r, float g, float b, float a);
 
 		static Color White();
-
 		static Color Black();
-
 		static Color Red();
-
 		static Color Green();
-
 		static Color Blue();
-
 		static Color Yellow();
-
 		static Color Clear();
+
+		Color operator+(Color other) const;
+		void operator+=(Color other);
+		Color operator-(Color other) const;
+		void operator-=(Color other);
+		Color operator*(Color other) const;
+		Color operator*(float scalar) const;
+		friend Color operator*(float scalar, Color color);
+		void operator*=(Color other);
+		void operator*=(float scalar);
+		Color operator/(Color other) const;
+		Color operator/(float scalar) const;
+		friend Color operator/(float scalar, Color color);
+		void operator/=(Color other);
+		void operator/=(float scalar);
+		bool operator>(Color other) const;
+		bool operator<(Color other) const;
+		bool operator==(Color other) const;
+		bool operator!=(Color other) const;
+
+		operator Vector4() const;
+
+		static Color Normalized(Color color);
+		static float Magnitude(Color color);
+		static float SqrMagnitude(Color color);
+		static float Dot(Color a, Color b);
+		static bool Approximately(Color a, Color b);
 	};
 }
